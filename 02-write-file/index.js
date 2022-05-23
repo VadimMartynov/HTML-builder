be092ab5bb002551eb4037f328401540df9c32ad
Vadim Martynov <bailmatezkij@gmail.com>
2022-05-23 22:55:06 +0300

    fix: fix all tasks according to requirements

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,12 +3,14 @@ const path = require('path');
 const fs = require('fs');
 stdout.write('Hi, enter text:\n');
 
-
+fs.appendFile(path.join(__dirname, 'text.txt'),'',err => {});
+fs.truncate(path.join(__dirname,'text.txt'), err => {});
 
 stdin.on('data', config => {
     let str = config.toString();
     str = str.trim();
     if (str == 'exit') {
+        stdout.write('Bye');
         process.exit();
     }
     fs.appendFile(
@@ -19,3 +21,8 @@ stdin.on('data', config => {
         }
     );
 });
+
+process.on('SIGINT', () => {
+    console.log('Bye');
+    process.exit();
+});  
